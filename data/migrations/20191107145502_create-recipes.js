6b8b79a82c02999355e9bcc7e6053865bec90daa
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-11-07 15:20:55 -0600

    created tables through knex

--- data/migrations/20191107145502_create-recipes.js
+++ data/migrations/20191107145502_create-recipes.js
@@ -1,6 +1,38 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+  return knex.schema
+  .createTable('recipes', tbl => {
+      tbl.increments();
+      tbl.string('recipe_name', 256)
+        .notNullable();
+  })
+  .createTable('ingredients', tbl => {
+    tbl.increments();
+    tbl.string('ingredient_name', 256)
+      .notNullable();
+})
+.createTable('steps', tbl => {
+    tbl.increments();
+    tbl.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes');
+})
+.createTable('recipe_ingredients', tbl => {
+    tbl.increments();
+    tbl.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes');
+    tbl.integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes');
+    tbl.float('quantity')
+})
 };
 
 exports.down = function(knex) {
